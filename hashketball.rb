0a7f755286b74c403819f03dab068587deb15c25
Arielle ross <arielle@arielleross.com>
2020-11-27 20:44:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -54,7 +54,7 @@ def game_hash
         :assists => 2,
         :steals => 4,
         :blocks => 11,
-        :slam_dunks => 1},
+        :slam_dunks => 1}
         
       
         ]
@@ -111,7 +111,7 @@ def game_hash
         :assists => 12,
         :steals => 7,
         :blocks => 5,
-        :slam_dunks => 12},
+        :slam_dunks => 12}
         ]
     }
   }
