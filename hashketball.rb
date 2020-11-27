fedcc06d0bbba978a5b9e6b82a380d16ecad843c
Arielle ross <arielle@arielleross.com>
2020-11-27 20:36:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,6 @@ def game_hash
         :steals =>3,
         :blocks => 1,
         :slam_dunks =>1},
-        binding.pry
         {:player_name =>"Reggie Evans",
         :number => 30,
         :shoe => 14,
