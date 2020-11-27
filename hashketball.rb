a9f413edfe496500cabcc60dfae3acdaea11968f
Arielle ross <arielle@arielleross.com>
2020-11-27 20:23:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,8 @@
 def game_hash
   hashketball = {
     :home =>{
-      :team_name =>{},
-      :colors => {},
+      :team_name =>{"Team Name"},
+      :colors => {["color1", "color2"]},
       :players => {}
     },
     :away=>{}
